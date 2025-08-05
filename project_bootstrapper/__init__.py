a93578214524cb07697d2c9db42aebd2004dbeeb
IH <147421162+ifhDev@users.noreply.github.com>
2025-08-06 00:31:04 +0200

    fixed typo

--- project_bootstrapper/__init__.py
+++ project_bootstrapper/__init__.py
@@ -1,7 +1,7 @@
 from .config import prompt_for_valid_directory, load_config
 from .project_data_templates import DATA_SCIENCE_FOLDERS, DATA_SCIENCE_FILES, get_lightweight_files, get_lightweight_folders, GITIGNORE_ENTRIES
 from .scaffold import create_structure
-from uv_init import dependency_check, init_project
+from .uv_init import dependency_check, init_project
 
 __all__ = [
     "prompt_for_valid_directory",
